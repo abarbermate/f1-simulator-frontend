b020ca3e4290a32ff96ebbbdbc71edcfb7d1a688
Barbér Máté <mate.barber@kifli.tech>
2024-03-06 09:12:20 +0100

    Update subtitle

--- src/app/drivers/page.tsx
+++ src/app/drivers/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={"flex min-h-screen flex-col items-center gap-10 p-10"}>
       <h1 className={"text-5xl font-bold"}>F1 Simulator</h1>
-      <h2 className={"text-2xl"}>Current order:</h2>
+      <h2 className={"text-2xl"}>Leaderboard:</h2>
       <div className={"flex flex-wrap items-center justify-center gap-10"}>
         <DriverCards />
       </div>
