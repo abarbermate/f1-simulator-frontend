3c9a357dad5349d34f4af9bbaaa0ccf839e516d8
Barbér Máté <mate.barber@kifli.tech>
2024-03-06 01:15:31 +0100

    Update next-config images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '1337',
+                pathname: '/static/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'flagsapi.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    }
+};
 
 export default nextConfig;
